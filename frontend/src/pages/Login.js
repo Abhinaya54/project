8ccb11d7364dc39de16afa4a08636d414f6f0b0a
abhinayapulagam5-svg <abhinayapulagam5@gmail.com>
2026-01-17 17:32:00 +0530

    Fix double slash in API URL for login endpoint

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
       formData.append("username", email);
       formData.append("password", password);
 
-      const response = await axios.post(`${API_BASE_URL}/auth/login`, formData, {
+      const response = await axios.post(`${API_BASE_URL}auth/login`, formData, {
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
         },
